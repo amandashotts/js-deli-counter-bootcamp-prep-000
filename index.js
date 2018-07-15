6515364e700139f1abc8e8bab4dbbd09d978eff7
Amanda S <shottsan@dukes.jmu.edu>
2018-07-15 18:46:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,13 +26,15 @@ function currentLine (line) {
   for (var i = 0; i < line.length; i++) {
     numberedLines.push(`${i+1}. ${line[i]}`);
     } 
-return `The line is currently: ${numberedLines.join(', ')}`
+
     if (line.length === 0) {
         return `The line is currently empty.` 
     }
-} 
+ else {
+  return `The line is currently: ${numberedLines.join(', ')}`
+}
 
-
+}
 
 /* 
 An alternative way to build currentLine:
