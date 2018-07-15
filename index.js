60776e126ba336e2e8ee60f9190d9206f64f75e1
Amanda S <shottsan@dukes.jmu.edu>
2018-07-15 18:46:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,13 +26,12 @@ let numberedLines = [];
   for (var i = 0; i < line.length; i++) {
     numberedLines.push(`${i+1}. ${line[i]}`);
   } 
-
   if (line.length === 0) {
-        return `The line is currently empty.` 
-    }
+    return `The line is currently empty.` 
+  }
  else {
-  return `The line is currently: ${numberedLines.join(', ')}`
-}
+    return `The line is currently: ${numberedLines.join(', ')}`
+  }
 }
 
 /* 
