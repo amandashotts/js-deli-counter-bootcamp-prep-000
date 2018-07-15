af6a2a0d3d62c153c38f72cd6dfd0bdc2bf6e9d2
Amanda S <shottsan@dukes.jmu.edu>
2018-07-15 18:46:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,18 +22,17 @@ function nowServing (line) {
 
 //Build a function currentLine that returns the current line. For example, if katzDeliLine is currently ["Ada", "Grace"], currentLine(katzDeliLine) would return "The line is currently: 1. Ada, 2. Grace". If there is nobody in line, it should return "The line is currently empty."
 function currentLine (line) {
-    let numberedLines = [];
+let numberedLines = [];
   for (var i = 0; i < line.length; i++) {
     numberedLines.push(`${i+1}. ${line[i]}`);
-    } 
+  } 
 
-    if (line.length === 0) {
+  if (line.length === 0) {
         return `The line is currently empty.` 
     }
  else {
   return `The line is currently: ${numberedLines.join(', ')}`
 }
-
 }
 
 /* 
